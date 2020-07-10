eb813efe571581a102a46a2fd602b1b53df19f79
henglihengli <lsliheng@gmail.com>
2020-07-09 19:52:09 -0700

    entity builder pattern

---
+++ src/main/java/entity/Item.java
@@ -0,0 +1,70 @@
+package entity;
+
+import java.util.Set;
+
+public class Item {
+	private String itemId;
+	private String name;
+	private String address;
+	private Set<String> keywords;
+	private String imageUrl;
+	private String url;
+	
+	private Item(ItemBuilder builder) {
+		this.itemId = builder.itemId;
+		this.name = builder.name;
+		this.address = builder.address;
+		this.keywords = builder.keywords;
+		this.imageUrl = builder.imageUrl;
+		this.imageUrl = builder.url;
+	}
+	
+	public String getItemId() {
+		return itemId;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public Set<String> getKeywords() {
+		return keywords;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public String getUrl() {
+		return url;
+	}
+	
+	public static class ItemBuilder {
+		private String itemId;
+		private String name;
+		private String address;
+		private Set<String> keywords;
+		private String imageUrl;
+		private String url;
+		public void setItemId(String itemId) {
+			this.itemId = itemId;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public void setKeywords(Set<String> keywords) {
+			this.keywords = keywords;
+		}
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public Item build() {
+			return new Item(this);
+		}
+	}
+}
