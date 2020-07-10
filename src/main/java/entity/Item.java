d10e5bb3cb0d7ff46b56841d1750e8dc2186300d
henglihengli <lsliheng@gmail.com>
2020-07-09 20:34:26 -0700

    entity builder lombok

--- src/main/java/entity/Item.java
+++ src/main/java/entity/Item.java
@@ -2,6 +2,12 @@ package entity;
 
 import java.util.Set;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+
 public class Item {
 	private String itemId;
 	private String name;
@@ -9,72 +15,4 @@ public class Item {
 	private Set<String> keywords;
 	private String imageUrl;
 	private String url;
-	
-	private Item(ItemBuilder builder) {
-		this.itemId = builder.itemId;
-		this.name = builder.name;
-		this.address = builder.address;
-		this.keywords = builder.keywords;
-		this.imageUrl = builder.imageUrl;
-		this.imageUrl = builder.url;
-	}
-	
-	public String getItemId() {
-		return itemId;
-	}
-	public String getName() {
-		return name;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public Set<String> getKeywords() {
-		return keywords;
-	}
-	public String getImageUrl() {
-		return imageUrl;
-	}
-	public String getUrl() {
-		return url;
-	}
-	
-	public static class ItemBuilder {
-		private String itemId;
-		private String name;
-		private String address;
-		private Set<String> keywords;
-		private String imageUrl;
-		private String url;
-		
-		public ItemBuilder() {
-		}
-		
-		public ItemBuilder setItemId(String itemId) {
-			this.itemId = itemId;
-			return this;
-		}
-		public ItemBuilder setName(String name) {
-			this.name = name;
-			return this;
-		}
-		public ItemBuilder setAddress(String address) {
-			this.address = address;
-			return this;
-		}
-		public ItemBuilder setKeywords(Set<String> keywords) {
-			this.keywords = keywords;
-			return this;
-		}
-		public ItemBuilder setImageUrl(String imageUrl) {
-			this.imageUrl = imageUrl;
-			return this;
-		}
-		public ItemBuilder setUrl(String url) {
-			this.url = url;
-			return this;
-		}
-		public Item build() {
-			return new Item(this);
-		}
-	}
 }
